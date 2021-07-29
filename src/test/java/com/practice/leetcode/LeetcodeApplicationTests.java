11ebdf988da9c2fa054ecc4d7716f8865e7e62f3
zhangshizhu <18230338980@163.com>
2021-07-29 22:41:51 +0800

    初始化

---
+++ src/test/java/com/practice/leetcode/LeetcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.leetcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeetcodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
